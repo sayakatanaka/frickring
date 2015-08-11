15006000a929b1be621ec7eb20c74ae7b264f8e1
sayakatanaka <sayaka.5mth@gmail.com>
2015-08-11 08:49:11 +0000

    images

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise'
  gem 'sdoc', '~> 0.4.0', group: :doc
- gem 'devise'
  gem 'paperclip', '~> 4.2'
 
 
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @pins = Pin.all
@@ -12,7 +12,7 @@ class PinsController < ApplicationController
 
  
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
  
@@ -21,7 +21,7 @@ class PinsController < ApplicationController
 
  
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.' 
       else
@@ -48,7 +48,11 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
